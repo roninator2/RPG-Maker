3197d8790002e427a8951f3bacc9eaecc62911f4
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-16 15:31:58 -0400

    Create TsukiHime Instance Items - Food Quality.rb

---
+++ VX-Ace/Patches/TsukiHime Instance Items - Food Quality.rb
@@ -0,0 +1,53 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Food Quality Patch for Hime Instance   ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Compatibility patch                         ║    08 Jun 2022     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: Hime Instance Items                                      ║
+# ║           Roninator2 - Food Quality                                ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Patch for Instance Items                                     ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Plug and Play                                                    ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 08 Jun 2022 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_fqphi] = 1.01        # Food Quality Patch for Hime Instance
+
+module InstanceManager
+  class << self; alias r2_setup_item_random_food_quality setup_item_instance; end
+  def self.setup_item_instance(obj)
+    value = obj.food_quality + 1
+    obj.food_quality = rand(value).to_i
+    r2_setup_item_random_food_quality(obj)
+  end
+end
