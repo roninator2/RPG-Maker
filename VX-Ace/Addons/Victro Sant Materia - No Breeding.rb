0e61d760326a668f284eb5fbc4a4f421f90e8cc6
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-18 17:54:41 -0400

    Create Victro Sant Materia - No Breeding.rb

---
+++ VX-Ace/Addons/Victro Sant Materia - No Breeding.rb
@@ -0,0 +1,57 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Materia No Breeding option             ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Alter Materia System                        ║    24 Jan 2022     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║        Adds no breed option                                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Use <no breed> to not have a materia not duplicate               ║
+# ║   when mastered.                                                   ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 24 Jan 2022 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_mnbo] = 1.01         # Materia No Breeding option
+
+class RPG::Armor < RPG::EquipItem
+  def gain_ap(n, growth)
+    old_level = level
+    @ap += (n * growth).to_i
+    breedoff = note =~ /<NO BREED>/i ? true : false
+    if Materia_Breeding && old_level < max_level && level == max_level
+      $game_party.gain_materia($data_armors[@id]) unless breedoff == true
+    end
+  end
+end
