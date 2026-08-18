a63a3e3e30e84a547fb5ae9606fe748f62a04cc2
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-18 18:29:50 -0400

    Create Play Sound on Close Window.rb

---
+++ VX-Ace/Process/Play Sound on Close Window.rb
@@ -0,0 +1,57 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Play Sound When Closing Message        ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Script Function                             ║    22 Apr 2020     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Play sound when closing the message window                   ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Configure sound file below                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 22 Apr 2020 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+ 
+module R2_MESSAGE_CLOSE
+	AUDSND = "Close1"	# must be in quotes
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_pswcm] = 1.01        # Play Sound When Closing Message
+
+class Window_Message < Window_Base
+  def close_and_wait
+    close
+    Fiber.yield until all_close?
+    RPG::SE.new(R2_MESSAGE_CLOSE::AUDSND, 80, 0).play
+  end
+end
