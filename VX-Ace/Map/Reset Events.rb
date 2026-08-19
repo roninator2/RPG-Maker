59db3d9b2acf6f887da199834b407ec8fac4d26f
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-19 18:52:39 -0400

    Create Reset Events.rb

---
+++ VX-Ace/Map/Reset Events.rb
@@ -0,0 +1,111 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Reset Self Switch for Events           ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║   Reset Self switch on events                 ╠════════════════════╣
+# ║   to have events come back                    ║    02 Jun 2023     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Remove event self switch                                     ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Change the Text used to stop resetting                           ║
+# ║     Text = "Your text"                                             ║
+# ║                                                                    ║
+# ║   Place a comment on the event page when the event is              ║
+# ║   not to reset. Must be on the active event page.                  ║
+# ║   For example. first page of event is an enemy                     ║
+# ║     Second page is self switch A - when enemy defeated             ║
+# ║     Second page is treasure or something else                      ║
+# ║     Third page is self switch B - when finished                    ║
+# ║     Third page has the comment listed below                        ║
+# ║                                                                    ║
+# ║     If the event is currently on the third page                    ║
+# ║     with the comment below, it will not reset                      ║
+# ║     but it if is on the second page, it will reset                 ║
+# ║     if the third page does not have the comment                    ║
+# ║     it will reset.                                                 ║
+# ║                                                                    ║
+# ║   Some coded copied from CSCA Dungeon Tools script                 ║
+# ║                                                                    ║
+# ║   Put in a script call on the event that will transfer             ║
+# ║   the player before the transfer command.                          ║
+# ║     script: map_reset                                              ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 02 Jun 2023 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_Skip_Event
+  Text = "SKIP_EVENT"
+ end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_rssfe] = 1.01        # Reset Self Switch for Events
+
+#==============================================================================
+# Game_Interpreter
+#==============================================================================
+class Game_Interpreter
+  def map_reset
+    if SceneManager.scene_is?(Scene_Map)
+      $game_map.map_reset
+    end
+  end
+end
+
+#==============================================================================
+# Game_Map
+#==============================================================================
+class Game_Map
+  def map_reset
+    event_amount = events.size
+    for i in 1..event_amount
+      next if event_skip(i)
+      switches = ["A","B","C","D"]
+      for j in switches
+        key = [@map_id, i, j]
+        $game_self_switches[key] = false
+      end
+    end
+  end
+  def event_skip(id)
+    events.each do |i, event|
+      next if event.nil?
+      if event.id == id
+        event.list.each do |comm|
+          if comm.code == 108 && comm.parameters == [R2_Skip_Event::Text]
+            return true
+          end
+        end
+      end
+    end
+    return false
+  end
+end
