808f3117feb2c395115e87d792bcfcf1a9dd5d4c
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-19 18:55:18 -0400

    Create Move Slow States.rb

---
+++ VX-Ace/Map/Move Slow States.rb
@@ -0,0 +1,64 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Move Slow States                       ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Add Slow                                    ║    11 Nov 2021     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       add slow down if sleep or paralysed                          ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Specify what states will cause slow movement                     ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 11 Nov 2021 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_Move_Slow_States
+  STATES = [6, 7, 8]
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_mss] = 1.01          # Move Slow States
+
+class Game_Player < Game_Character
+  def real_move_speed
+    speed = @move_speed + (dash? ? 1 : 0)
+    R2_Move_Slow_States::STATES.each { |id|
+      for i in 1..$game_party.members.size - 1
+        actor = $game_party.members[i]
+        if actor.state?(id)
+          speed -= 0.5
+        end
+      end
+    }
+    return speed
+  end
+end
