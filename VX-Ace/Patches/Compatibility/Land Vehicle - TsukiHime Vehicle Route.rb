a55fc60295afbc5db5e74ca1e800b9cb62e86c98
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-19 18:56:51 -0400

    Create Land Vehicle - TsukiHime Vehicle Route.rb

---
+++ VX-Ace/Patches/Compatibility/Land Vehicle - TsukiHime Vehicle Route.rb
@@ -0,0 +1,64 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Tsukihime land vehicle patch           ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║           Patch                               ║    20 Jul 2023     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: Roninator2 Land Vehicle                                  ║
+# ║           Tsukihime Vehicle Route script                           ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║              Fix move routes for script                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Plug and play                                                    ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 20 Jul 2023 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_tlvp] = 1.01         # Tsukihime land vehicle patch
+
+#-------------------------------------------------------------------------------
+# Since the vehicles will be moving by themselves, they should follow their
+# own passage rules
+#-------------------------------------------------------------------------------
+class Game_Vehicle < Game_Character
+  def map_passable?(x, y, d)
+    case @type
+    when :land
+      return $game_map.land_passable?(x, y, d)
+    when :boat
+      return $game_map.boat_passable?(x, y)
+    when :ship
+      return $game_map.ship_passable?(x, y)
+    when :airship
+      return true
+    end
+  end
+end
