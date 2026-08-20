fa6e56d67ef76d5b2cb0e65486c0d76e724874cd
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-19 22:30:33 -0400

    Create Game Over on Death with State.rb

---
+++ VX-Ace/Process/Battle/Game Over on Death with State.rb
@@ -0,0 +1,83 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Game Over on Death with State          ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Overwrite Game Over Processes               ║    31 Dec 2021     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Call Game Over when state is inflicted                       ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Configure settings below                                         ║
+# ║   Specify the state that will call a game over                     ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 31 Dec 2021 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_State_GameOver
+  States = [4, 7]
+  Message = "died in a frenzie."
+  Act_Name = true  # use actor name in message
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_gowsond] = 1.01      # Game Over on Death with State
+
+module BattleManager
+  def self.turn_end
+    @phase = :turn_end
+    @preemptive = false
+    @surprise = false
+    call_dead_msg
+  end
+  def self.call_dead_msg
+    actbat = nil
+    actname = nil
+    dthmsg = nil
+    @msgcnt = 0 if @msgcnt == nil
+    $game_party.battle_members.each do |act|
+      if act.dead?
+        actbat = $game_actors[act.id]
+        actname = act.name
+        dthmsg = R2_State_GameOver::Message
+        dthmsg = "#{actname} " + R2_State_GameOver::Message if R2_State_GameOver::Act_Name == true
+        @msgcnt += 1
+      end
+      if actbat != nil
+        if actbat.dead? && (actbat.states.any? { |st| R2_State_GameOver::States.include?(st.id) } )
+          $game_message.add(sprintf(dthmsg, actname)) if @msgcnt >= 1
+          wait_for_message
+          @msgcnt = 0
+          SceneManager.goto(Scene_Gameover) 
+        end
+      end
+    end
+  end
+end
