f917bd33ecb806269327068d057ee9564efe272f
roninator2 <50345160+roninator2@users.noreply.github.com>
2026-08-19 22:49:06 -0400

    Create After Death call common event and transfer to a specific map without variables.rb

---
+++ VX-Ace/Process/Battle/After Death call common event and transfer to a specific map without variables.rb
@@ -0,0 +1,96 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Call CE Transfer without variables     ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Script Function                             ║    30 Sep 2020     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Call Common Event when death occurs                          ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Configure settings below                                         ║
+# ║   Set map id and position to return to after death                 ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 30 Sep 2020 - Script finished                               ║
+# ║ 1.01 - 14 Mar 2026 - Added Import Value                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_nodie
+	CEID = 1			# common event id
+	UseCE = 2			# switch to call common event
+	Rewards = 1		# switch to activate this script
+	Map = 2				# map id to transfer to
+	X_spot = 10		# player x position
+	Y_spot = 20		# player y position
+	Dir		 = 4		# player direction facing
+end
+
+module Vocab
+	Livetext = "Barely made it out!"
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+$imported = {} if $imported.nil?
+$imported[:r2_ccetwv] = 1.01       # Call CE Transfer without variables
+
+module BattleManager
+  def self.process_defeat
+		if $game_switches[R2_nodie::Rewards] == true
+			$game_message.add(sprintf(Vocab::Livetext, $game_party.name))
+			wait_for_message
+			revive_battle_members
+			display_exp
+			gain_gold
+			gain_drop_items
+			gain_exp
+			replay_bgm_and_bgs
+			SceneManager.return
+			map_id = R2_nodie::Map
+			x = R2_nodie::X_spot
+			y = R2_nodie::Y_spot
+			direction = R2_nodie::Dir
+			$game_player.reserve_transfer(map_id, x, y, direction)
+			$game_player.perform_transfer
+			$game_temp.reserve_common_event(R2_nodie::CEID) if $game_switches[R2_nodie::UseCE] == true
+			battle_end(0)
+			return true
+		else
+			$game_message.add(sprintf(Vocab::Defeat, $game_party.name))
+			wait_for_message
+			if @can_lose
+				revive_battle_members
+				replay_bgm_and_bgs
+				SceneManager.return
+			else
+				SceneManager.goto(Scene_Gameover)
+			end
+			battle_end(2)
+			return true
+		end
+  end
+end
